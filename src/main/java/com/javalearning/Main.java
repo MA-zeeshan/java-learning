546b91824406fe21565b4f66af68e731b1c65827
MA-zeeshan <m.awais.zeeshan7262@gmail.com>
2024-10-07 19:59:20 +0500

    practice

--- src/main/java/com/javalearning/Main.java
+++ src/main/java/com/javalearning/Main.java
@@ -1,19 +1,29 @@
 package com.javalearning;
 
-import java.text.NumberFormat;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-int temp = 0;
-if(temp>30 && temp<50){
-    System.out.println("today is hot");
-}else if(temp<30){
-    System.out.println("today is Cold");
-    }else{
-    System.out.println("day is burning today!!! drink some water...");
-}
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int numberInput = scanner.nextInt();
+  if((numberInput % 5==0) && (numberInput % 3==0)){
+            System.out.println("fizzbuzz");
+            return;
+        }
+        if(numberInput % 5==0){
+            System.out.println("fizz");
+            return;
+        }
+
+
+
+         if(numberInput % 3==0){
+            System.out.println("buzz");
+            return;
+        }
+        System.out.println(numberInput);
     }
 }
